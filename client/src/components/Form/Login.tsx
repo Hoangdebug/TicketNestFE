9a02f78ca8fb37789d345476aeacf500d9e85038
Quang Minh <qminh12300@gmail.com>
2024-05-25 19:20:11 +0700

    Add box shadow and display flex

--- client/src/components/Form/Login.tsx
+++ client/src/components/Form/Login.tsx
@@ -3,18 +3,24 @@ import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 const LoginForm: ILoginComponent<ILoginComponentProps> = () => {
   return (
     <div className="components__login">
-    <div id="components__login-form">
-        <h2>Login</h2>
+    <div className="components__login-form p-4 ">
+        <h2 className='fw-bold text-center'>Sign in</h2>
         <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" className="form-control" id="username" name="username" required />
+            <label htmlFor="username">Mail</label>
+            <input type="text" className="form-control" id="username" name="username" required placeholder='Enter Mail'/>
         </div>
         <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" id="password" name="password" required />
+            <label htmlFor="password">Password
+            <span className='text-danger'>*</span>
+            </label>
+            <input type="password" className="form-control" id="password" name="password" required placeholder='Enter password'/>
             <RemoveRedEyeIcon className='hehe'/>
         </div>
-        <button type="submit" className="btn btn-primary btn-block">Login</button>
+        <div className='d-flex flex-row'>
+          <input id="danger" type="checkbox" />
+          <label htmlFor="danger">Remember me</label>
+        </div>
+        <button type="submit" className="btn btn-primary btn-block">Sign in</button>
         <p id="error-message" className="error-message text-danger mt-2"></p>
     </div>
 </div>
