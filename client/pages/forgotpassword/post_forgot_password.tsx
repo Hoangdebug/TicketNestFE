b9f31a06274e60b88e8a7ad6fc9582268d8fa23d
Quang Minh <qminh12300@gmail.com>
2024-06-06 21:28:16 +0700

    Basic design of sidebar

--- client/pages/forgotpassword/post_forgot_password.tsx
+++ client/pages/forgotpassword/post_forgot_password.tsx
@@ -9,9 +9,9 @@ const PostForgotPasswordPage: IPostForgotPasswordPage<IPostForgotPasswordPagePro
             <div className="pages__postForgotPassword-leftSide ">
                 <img className="pages__postForgotPassword-leftSide-logo" style={{ height: '100vh' }} src={images.LOGIN_LOGO} alt="" />
             </div>
-            <div className="col-12 col-md-6 pages__postForgotPassword-rightSide d-flex flex-column justify-content-center">
+            {/* <div className="col-12 col-md-6 pages__postForgotPassword-rightSide d-flex flex-column justify-content-center">
                 <PostForgotPasswordForm />
-            </div>
+            </div> */}
             <div className="col-md-6 col-sm-12 pages__postForgotPassword-rightSide ">
                 <img
                     className="pages__postForgotPassword-rightSide-logo"
