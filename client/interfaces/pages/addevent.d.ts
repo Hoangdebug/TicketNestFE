4353a70f111be0923b280860916fb1d2b04428ae
Anh <ngocanhtk04@gmail.com>
2024-06-09 19:52:24 +0700

    feature: design list and add event

---
+++ client/interfaces/pages/addevent.d.ts
@@ -0,0 +1,5 @@
+import { IBasePageProps, IBasePage } from '@interfaces/pages/base';
+
+interface IAddEventPageProps extends IBasePageProps {}
+
+interface IAddEventPage<P = {}> extends IBasePage<P> {}
