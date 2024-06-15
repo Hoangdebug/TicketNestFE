db5c3251e8b3e22840622fce7759737a36d9c572
Trungnb <trungnb@vnext.vn>
2024-06-15 09:29:29 +0700

    design home

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,6 +6,7 @@ import { authHelper } from '@utils/helpers';
 import NavBar from '@components/layouts/NavBar';
 import { routes } from '@utils/constants';
 import { EventList } from '@components/index';
+import Tabs from '@components/commons/Tab';
 
 const HomePage: IHomePage<IHomePageProps> = () => {
     const router = useRouter();
@@ -29,7 +30,45 @@ const HomePage: IHomePage<IHomePageProps> = () => {
             <div className="py-5">
                 <EventList />
             </div>
-            <div className=""></div>
+            <div className="pages__home--host py-5 bases__background--white d-flex jus">
+                <div className="p-5">
+                    <h3 className="bases__font--40 fw-bolder">Host Engaging Online and Venue Events with Barren</h3>
+                    <p className="m-0 bases__font--20 text-wrap">
+                        Organise venue events and host online events with unlimited possibilities using our built-in virtual event platform.
+                        Build a unique event experience for you and your attendees.
+                    </p>
+                </div>
+            </div>
+            <div className="py-5 pages__home--step">
+                <div className="p-5">
+                    <Tabs
+                        type="material"
+                        defaultActiveKey="step1"
+                        tabs={[
+                            {
+                                title: 'Step 01',
+                                event: 'step1',
+                                content: 'hdhdhdhd',
+                            },
+                            {
+                                title: 'Step 02',
+                                event: 'step2',
+                                content: 'hehehehe',
+                            },
+                            {
+                                title: 'Step 03',
+                                event: 'step3',
+                                content: 'hoohohohohoho',
+                            },
+                            {
+                                title: 'Step 04',
+                                event: 'step4',
+                                content: 'hihihhihih',
+                            },
+                        ]}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
