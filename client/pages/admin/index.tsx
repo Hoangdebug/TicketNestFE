f8d5ac44fd8120c11a0820b17e409fd5bd45b95d
Trungnb <trungnb@vnext.vn>
2024-06-23 13:47:38 +0700

    fix code

--- client/pages/admin/index.tsx
+++ client/pages/admin/index.tsx
@@ -1,8 +1,4 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
-import { ReduxStates } from '@redux/reducers';
-import { IAdminPage, IAdminPageProps, IAdminPageState } from '@interfaces/pages/admin';
+import { IAdminPage, IAdminPageProps } from '@interfaces/pages/admin';
 import Graphic_Side from '@components/layouts/admin/Graphic_Side';
 import Footer_Admin from '@components/layouts/admin/Footer_Admin';
 import SideBar from '@components/layouts/admin/Sidebar';
@@ -15,33 +11,25 @@ import Enterprise_Clients from '@components/layouts/admin/Enterprise_Clients';
 import Rightside_Content from '@components/layouts/Rightside_content';
 
 const AdminPages: IAdminPage<IAdminPageProps> = () => {
-    const { profile } = useSelector((states: ReduxStates) => states);
-    const router = useRouter();
-
     return (
         <div className="pages__admin row">
             <div className="pages__admin-content col-md-2 p-4 col-sm-12">
                 <SideBar />
             </div>
             <div className="pages__admin-main col-md-10 col-sm-12">
-                {/* Main content area */}
                 <div className="pages__admin-main-graphic">
-                    {/* Graphic side content */}
                     <Graphic_Side />
                 </div>
                 <div className="pages__admin-main-content row">
                     <div className="col-md-8 col-sm-12">
-                        {/* Left side content */}
                         <br></br>
                         <Main_Chart />
                         <br></br>
                         <div className="row">
                             <div className="col-md-6 col-sm-12">
-                                {' '}
                                 <Earnings /> <br></br>
                             </div>
                             <div className="col-md-6 col-sm-12">
-                                {' '}
                                 <Conversions /> <br></br>{' '}
                             </div>
                         </div>
@@ -49,8 +37,6 @@ const AdminPages: IAdminPage<IAdminPageProps> = () => {
                         <br></br>
                     </div>
                     <div className="col-md-4 col-sm-12">
-                        {/* Right side content */}
-
                         <Rightside_Content />
                         <br></br>
                         <Visitor />
@@ -60,7 +46,6 @@ const AdminPages: IAdminPage<IAdminPageProps> = () => {
                     </div>
                 </div>
                 <div className="pages__admin-main-footer">
-                    {/* Footer content */}
                     <Footer_Admin />
                 </div>
             </div>
