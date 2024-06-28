037fbcdbb832b7ed291712316cf090812fd3bf25
Quang Minh <qminh12300@gmail.com>
2024-06-28 12:45:57 +0700

    alter scss contac page for second time

--- client/components/forms/Contact.tsx
+++ client/components/forms/Contact.tsx
@@ -2,32 +2,32 @@ import React from 'react';
 
 const ContactForm: IContactComponent<IContactComponentProps> = () => {
     return (
-        <div className="contact">
-            <form className="contact-form">
-                <div className="contact-form-form-group">
+        <div className="components__contact">
+            <div className="components__contact-form">
+                <div className="components__contact-form-group">
                     <label>First Name*</label>
                     <input type="text" name="firstName" required={true} />
                 </div>
-                <div className="contact-form-form-group">
+                <div className="components__contact-form-group">
                     <label>Last Name*</label>
                     <input type="text" name="lastName" required={true} />
                 </div>
-                <div className="contact-form-form-group">
+                <div className="components__contact-form-group">
                     <label>Email*</label>
                     <input type="email" name="email" required={true} />
                 </div>
-                <div className="contact-form-form-group">
+                <div className="components__contact-form-group">
                     <label>Phone*</label>
                     <input type="tel" name="phone" required={true} />
                 </div>
-                <div className="contact-form-form-group full-width">
+                <div className="components__contact-form-group full-width">
                     <label>Message*</label>
                     <textarea name="message" required={true}></textarea>
                 </div>
-                <div className="contact-form-form-group full-width">
+                <div className="components__contact-form-group full-width">
                     <button type="submit">Submit</button>
                 </div>
-            </form>
+            </div>
         </div>
     );
 };
